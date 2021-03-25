f2459b7a64cc427080cffebaf11d2e8eb20de6c8
Legacy <yugi.t@qq.com>
2021-03-25 22:33:47 +0800

    Never say never

--- Code/ch1/main.cpp
+++ Code/ch1/main.cpp
@@ -1,6 +1,11 @@
 #include "Game.hpp"
+#include <iostream>
+using namespace std;
 int main() {
   Game game;
   game.run();
+  for (int i = 0; i < 10; ++i) {
+    cout << "You're really special" << endl;
+  }
   return 0;
 }
