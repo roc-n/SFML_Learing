5e4c0b7571ac4586717314ce1b202888de0f1b7d
Legacy <yugi.t@qq.com>
2021-04-13 17:04:15 +0800

    script

--- SFML-Game-Development-Book/03_World/Source/Main.cpp
+++ SFML-Game-Development-Book/03_World/Source/Main.cpp
@@ -1,13 +1,18 @@
 #include <Book/Game.hpp>
 
-#include <iostream>
 #include <stdexcept>
+#include <iostream>
 
-int main() {
-  try {
-    Game game;
-    game.run();
-  } catch (std::exception &e) {
-    std::cout << "\nEXCEPTION: " << e.what() << std::endl;
-  }
+
+int main()
+{
+	try
+	{
+		Game game;
+		game.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "\nEXCEPTION: " << e.what() << std::endl;
+	}
 }
