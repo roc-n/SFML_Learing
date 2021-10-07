d040162b1c6b1ab16705b2978eb1b10ac3c81cfd
roc <yugi.t@qq.com>
2021-10-07 17:27:05 +0800

    message

---
+++ Code/ch7/yaml.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+
+int main() {
+  YAML::Emitter out;
+  out << "Hello,World!";
+  std::cout << "Here's the output YAML:\n" << out.c_str() << "\n";
+  return 0;
+}
